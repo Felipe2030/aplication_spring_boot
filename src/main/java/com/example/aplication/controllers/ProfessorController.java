3d798fe012894ca626c4f5eb1cff681ba7c08a9f
Felipe2030 <win2016_br@outlook.com>
2024-01-11 16:22:07 -0300

    .

--- src/main/java/com/example/aplication/controllers/ProfessorController.java
+++ src/main/java/com/example/aplication/controllers/ProfessorController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.aplication.entities.Aluno;
 import com.example.aplication.entities.Professor;
 import com.example.aplication.services.ProfessorService;
 
